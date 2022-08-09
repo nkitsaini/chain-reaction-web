307e9b3242e66fd0f6bd1aa54ad6e46697544bbc
Ankit Saini <nnkitsaini@gmail.com>
2022-08-09 19:50:12 +0530

    cp

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,203 +1,166 @@
 <script lang="ts">
-	import Box from '$lib/Box.svelte';
+	import Konva from 'konva';
+	import { onMount } from 'svelte';
+	import { draw } from 'svelte/transition';
 	import type * as m from '$lib/models';
 	import * as u from '$lib/utils';
-	import { quintOut } from 'svelte/easing';
+	import * as g from '$lib/game_utils';
+	import Box from '$lib/Box.svelte';
+	import { Tween } from 'konva/lib/Tween';
+	const GRID_COLOR = '#d1d5db';
 
-	const width: number = 10;
-	const height: number = 10;
-
-	import { crossfade } from 'svelte/transition';
-	let [send, recieve] = crossfade({
-		duration: 100
-	});
-	let _ball_ids: number = 0;
-	function new_ball(): m.Ball {
-		return { id: _ball_ids++ };
-	}
-	function new_game(): m.Game {
-		return {
-			grid: u.create_empty_grid(height, width, () => ({ balls: [], player: null })),
-			curr_player: 0,
-			total_players: 2,
-			turn: 0
-		};
-	}
-
-	let game: m.Game = new_game();
-	$: grid = game.grid;
-	let blasting: boolean = false;
-
-	function get_max_allowed_balls(i: number, j: number) {
-		let i_at_ends = [0, height - 1].includes(i);
-		let j_at_ends = [0, width - 1].includes(j);
-		if (i_at_ends && j_at_ends) {
-			return 1;
-		} else if (i_at_ends || j_at_ends) {
-			return 2;
-		} else {
-			return 3;
+	class Id {
+		static box(row, col): string {
+			return `Box_${row}_${col}`;
+		}
+		static text(row, col): string {
+			return `Text_${row}_${col}`;
+		}
+		static group(row, col): string {
+			return `Group_${row}_${col}`;
 		}
 	}
-	function is_inside_grid(i: number, j: number) {
-		return i >= 0 && i <= height - 1 && j >= 0 && j <= width - 1;
-	}
-	const directions = [
-		[-1, 0],
-		[1, 0],
-		[0, -1],
-		[0, 1]
-	];
 
-	async function blast(delay: number = 200) {
-		console.assert(blasting);
-		let old_grid = u.create_empty_grid(height, width, () => ({ balls: [], player: null }));
-		for (const i of Array(height).keys()) {
-			for (const j of Array(width).keys()) {
-				old_grid[i][j] = {
-					balls: grid[i][j].balls.map((x) => x),
-					player: grid[i][j].player
-				};
-			}
-		}
+	type Context = CanvasRenderingContext2D;
+	type Point = [number, number];
 
-		for (const i of Array(height).keys()) {
-			for (const j of Array(width).keys()) {
-				if (get_max_allowed_balls(i, j) >= old_grid[i][j].balls.length) {
-					continue;
-				}
-				for (const [di, dj] of directions) {
-					let new_i = i + di;
-					let new_j = j + dj;
-					if (!is_inside_grid(new_i, new_j)) {
-						continue;
-					}
-					if (old_grid[i][j].balls.length == 0) {
-						grid[new_i][new_j].balls.push(new_ball());
-					} else {
-						grid[new_i][new_j].balls.push(grid[i][j].balls.pop());
-						while (grid[new_i][new_j].balls.length > get_max_allowed_balls(i + di, j + dj) + 1) {
-							grid[new_i][new_j].balls.pop();
-						}
-					}
-					grid[i][j].player = null;
-					grid[i + di][j + dj].player = game.curr_player;
-				}
-			}
-		}
-		let blast_remaining = false;
-		if (!has_won()) {
-			grid.forEach((row, i) => {
-				row.forEach((cell, j) => {
-					if (cell.balls.length > get_max_allowed_balls(i, j)) {
-						blast_remaining = true;
-					}
-				});
-			});
-		}
-		game = game;
-		if (blast_remaining) {
-			await new Promise((r) => {
-				setTimeout(async () => {
-					await blast(delay);
-					r(null);
-				}, delay);
-			});
-		}
-	}
-	function has_won(): boolean {
-		console.log('Checking won');
-		let won = true;
-		// console.log('Won', won, game.curr_player, game.turn);
-		if (game.turn == 0) {
-			console.log('game turn 0');
-			return false;
-		}
-		grid.forEach((row, i) => {
-			row.forEach((box, j) => {
-				if (box.balls.length != 0 && box.player != game.curr_player) {
-					won = false;
-				}
-			});
-		});
-		console.log('Won', won, game.curr_player, game.turn);
-		return won;
-	}
+	const canvas_width = 700;
+	const canvas_height = 700;
+	const rows = 10;
+	const cols = 10;
 
-	async function handle_click(i: number, j: number) {
+	function new_game(): g.Game {
+		return new g.Game({ rows, cols });
+	}
+	let game = new_game();
+
+	function get_box(row: number, col: number): [Point, Point] {
+		let y1 = (canvas_height * row) / rows;
+		let x1 = (canvas_width * col) / cols;
+		let y2 = (canvas_height * (row + 1)) / rows;
+		let x2 = (canvas_width * (col + 1)) / cols;
+		return [
+			[x1, y1],
+			[x2, y2]
+		];
+	}
+	let stage: Konva.Stage;
+	let layer: Konva.Layer;
+	let blasting = false;
+
+	function get_box_center(row: number, col: number): Point {
+		let [start, end] = get_box(row, col);
+		return [(start[0] + end[0]) / 2, (start[1] + end[1]) / 2];
+	}
+	async function handle_click(row: number, col: number) {
 		if (blasting) {
 			return;
 		}
-		let box = grid[i][j];
-		if (box.player != null && box.player != game.curr_player) {
+		let group = stage.findOne<Konva.Group>('#' + Id.group(row, col));
+		let text = stage.findOne<Konva.Text>('#' + Id.text(row, col));
+		let new_ball = game.tap(row, col);
+		function update_cell(i, j) {
+			let elm = stage.findOne<Konva.Text>('#' + Id.text(i, j));
+			elm.text(`o`.repeat(game.grid[i][j].balls.length));
+			elm.fill(u.player_colors[game.grid[i][j].player]);
+		}
+		if (new_ball == null) {
 			return;
 		}
-
-		box.player = game.curr_player;
-		box.balls.push(new_ball());
-
 		blasting = true;
-		await blast();
-		blasting = false;
+		update_cell(row, col);
+		let finished = false;
+		while (!finished) {
+			let res = game.blast_one();
+			finished = res.is_finished;
+			let tweens: Konva.Tween[] = [];
+			for (const move of res.moves) {
+				let [pi, pj] = move.prev_pos;
+				let [ni, nj] = move.next_pos;
+				update_cell(pi, pj);
 
-		let won = has_won();
-
-		if (won) {
-			alert(`player ${game.curr_player} won`);
+				let [ci, cj] = get_box_center(pi, pj);
+				let [ti, tj] = get_box_center(ni, nj);
+				let dummy_ball = new Konva.Circle({ x: ci, y: cj, radius: 3, fill: 'black' });
+				layer.add(dummy_ball);
+				tweens.push(
+					new Tween({
+						node: dummy_ball,
+						x: ti,
+						y: tj,
+						opacity: 0
+					})
+				);
+			}
+			for (const t of tweens) {
+				t.play();
+			}
+			await new Promise((r) => setTimeout(r, 200));
+			for (const t of tweens) {
+				t.node.destroy();
+				t.destroy();
+			}
+			for (const move of res.moves) {
+				let [ni, nj] = move.next_pos;
+				update_cell(ni, nj);
+			}
+		}
+		if (game.has_won()) {
+			alert(`Winner: ${game.curr_player + 1}:${u.player_colors[game.curr_player]}`);
+			stage.destroy();
+			initialize();
 			game = new_game();
-			return;
+		} else {
+			game.next_player();
 		}
-
-		game.curr_player = game.curr_player + 1;
-		if (game.curr_player >= game.total_players) {
-			game.curr_player = 0;
-			game.turn++;
-		}
-		game = game;
-		// TODO: Execute Blast
+		blasting = false;
 	}
-	// u.
+
+	function initialize() {
+		stage = new Konva.Stage({
+			container: container,
+			width: canvas_width,
+			height: canvas_height
+		});
+		layer = new Konva.Layer();
+		for (const i of Array(rows).keys()) {
+			for (const j of Array(cols).keys()) {
+				let [start, end] = get_box(i, j);
+				let group = new Konva.Group({
+					id: Id.group(i, j),
+					x: start[0],
+					y: start[1]
+				});
+				let box = new Konva.Rect({
+					id: Id.box(i, j),
+					width: end[0] - start[0],
+					height: end[1] - start[1],
+					stroke: GRID_COLOR
+				});
+
+				let text = new Konva.Text({
+					id: Id.text(i, j),
+					text: ' ',
+					align: 'center',
+					verticalAlign: 'middle',
+					height: canvas_height / rows,
+					width: canvas_width / cols,
+					fontSize: 16
+				});
+
+				group.add(text);
+				group.add(box);
+				box.on('click', (x) => handle_click(i, j));
+				layer.add(group);
+			}
+		}
+		stage.add(layer);
+	}
+
+	let container;
+	onMount(async () => {
+		initialize();
+	});
 </script>
 
-<button
-	on:click={() => {
-		grid.forEach((row, i) => {
-			row.forEach((box, j) => {
-				let max_balls = get_max_allowed_balls(i, j);
-				let player = Math.floor(Math.random() * 2);
-				console.log(row, j);
-				while (row[j].balls.length < max_balls) {
-					row[j].balls.push(new_ball());
-				}
-				row[j].player = player;
-				// row[j] = {
-				// 	balls: Array(max_balls).map(() => new_ball()),
-				// 	player: player
-				// };
-			});
-		});
-		game.turn = 2;
-		grid = grid;
-	}}>Populate</button
->
-<div class="shadow {u.get_player_shadow_class(game.curr_player)} shadow-lg">
-	<div class="select-none">
-		{#each grid as row, i (i)}
-			<div>
-				{#each row as box, j (j)}
-					<Box {box} cf={[send, recieve]} on:click={() => handle_click(i, j)} />
-				{/each}
-			</div>
-		{/each}
-	</div>
-
-	<div class="random hidden" />
-</div>
-
-<style>
-	/* This is to load tailwind classes which are getting used dynamically */
-	.random {
-		@apply text-green-500 text-blue-500 text-red-500;
-		@apply shadow-green-300 shadow-blue-300 shadow-red-300;
-	}
-</style>
+<div bind:this={container} />
