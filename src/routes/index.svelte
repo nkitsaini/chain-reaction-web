71ee33a94a65586a3956c69994124f80e1cbf126
Ankit Saini <nnkitsaini@gmail.com>
2022-08-10 19:01:14 +0530

    cp

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -10,6 +10,18 @@
 		rows: 10,
 		cols: 5
 	};
+	let max_players = Object.keys(u.player_colors).length - 1;
+	function changed() {
+		options = options;
+	}
+	function _options_valid(options) {
+		return (
+			2 <= options.players &&
+			options.players <= max_players &&
+			options.rows >= 5 &&
+			options.cols >= 5
+		);
+	}
 	$: height = browser ? window.innerHeight - 15 : 50;
 	$: width = browser ? window.innerWidth - 15 : 50;
 	function box_size() {
@@ -36,33 +48,47 @@
 		/>
 		<button
 			on:click={() => {
-				playing = false;
+				window.location.reload();
 			}}>Reset</button
 		>
 	</div>
 {:else}
 	<form class="max-w-[150px] p-3">
 		<label>
-			Players: <input
+			Players(2-7): <input
 				class="num_input"
 				type="number"
 				min="2"
-				max={Object.keys(u.player_colors).length - 1}
+				max={max_players}
 				bind:value={options.players}
+				on:change={changed}
 			/>
 		</label>
 		<br />
 		<label>
-			Rows: <input class="num_input" type="number" min="5" bind:value={options.rows} />
+			Rows(min 5): <input
+				class="num_input"
+				type="number"
+				min="5"
+				bind:value={options.rows}
+				on:change={changed}
+			/>
 		</label>
 		<br />
 		<label>
-			Columns: <input class="num_input" type="number" min="5" bind:value={options.cols} />
+			Columns(min 5): <input
+				class="num_input"
+				type="number"
+				min="5"
+				bind:value={options.cols}
+				on:change={changed}
+			/>
 		</label>
 		<br />
 		<input
 			type="submit"
-			class="bg-blue-500 px-3 py-1 rounded text-gray-100"
+			class="bg-blue-500 px-3 py-1 rounded text-gray-100 disabled:bg-gray-500"
+			disabled={!_options_valid(options)}
 			on:click={() => {
 				playing = true;
 			}}
